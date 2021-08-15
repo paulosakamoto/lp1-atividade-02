ea9158d8ce3d59da8aac7c8eacddd4a4cd97d042
Paulo Sakamoto <paas@hotmail.com>
2021-08-15 10:06:46 -0300

    Atividade 02 completa

---
+++ atividade-02-cout.c
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<"Meu nome é Paulo Sakamoto e moro em Balneário Camboriú.";
+    return 0;
+}
---
+++ atividade-02-printf.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Meu nome é Paulo Sakamoto e moro em Balneário Camboriú.");
+    return 0;
+}
